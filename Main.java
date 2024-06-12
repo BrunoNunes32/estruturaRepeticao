7b8d4334412400513c9043b3dc64b23eede21cd7
Bruno Nunes <brunorick32@hotmail.com>
2024-06-12 17:45:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    // AULA DE ESTRUTURA DE REPETIÇÃO 01/05 (FOR)
+    // for(int cont = 0; cont < 10; cont++){
+    // System.out.println("Valor do contador: " + cont);
+    // }
+    // for(int cont = 5; cont <= 20; cont++){
+    // System.out.println("Valor do contador: " + cont);
+    // }
+
+    // AULA DE ESTRUTURA DE REPETIÇÃO 02/05 (WHILE)
+    // int opcao = 0;
+    // Scanner entrada = new Scanner(System.in);
+    //ELE VERIFICA A CONDIÇÃO DEPOIS EXECUTA O CODIGO
+    // while (opcao != 99) {
+    //   System.out.println("Digite um valor qualquer ou 99 para sair");
+    //   opcao = entrada.nextInt();
+    // }
+    // entrada.close();
+
+    // AULA DE ESTRUTURA DE REPETIÇÃO 03/05 (DO/WHILE)
+    // int opcao;
+    // Scanner entrada = new Scanner(System.in);
+    // ELE EXECUTA O CODIGO DEPOIS VERIFICA A CONDIÇÃO
+    // do {
+    //   System.out.println("Digite um valor qualquer ou 99 para sair");
+    //   opcao = entrada.nextInt();
+    // }while(opcao != 99)
+
+      // AULA DE ESTRUTURA DE REPETIÇÃO 04/05 (ANINHADA)
+      // for(int i = 0; i<=10; i++){
+      //     for(int j =0; j<=10; j++){
+      //       System.out.println(i + " x " + j + " = " + (i*j));
+      //     }
+      //   System.out.println("--------------------");
+      // }
+
+    // AULA DE ESTRUTURA DE REPETIÇÃO 04/05 (METODOS)
+  //  int total = Main.somar(10, 50);
+  //     System.out.println("Total: " + total);
+  // }
+  //     // AULA DE ESTRUTURA DE REPETIÇÃO 04/05 (METODOS)
+  //     public static int somar(int a, int b){
+
+  //       return (a + b);
+
+    //DESAFIO 03: FAZER UM PROGRAMA QUE RECEBA UM NUMERO E MOSTRE O FATORIAL DELE
+
+    //DESAFIO 04: FAZER UM PROGRAMA QUE O USUARIO ESCOLHA A OPERAÇÃO E RECEBA DOIS NUMEROS E MOSTRE O RESULTADO
+
+      }
+
+}
